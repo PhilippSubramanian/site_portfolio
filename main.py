dd74f1ae6c1cb1a9839247016b9f0e087e88f9e8
PhilippSubramanian <itmightbegood16@gmail.com>
2024-12-29 12:03:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 #Импорт
-from flask import Flask, render_template, request, redirect
-
+from flask import Flask, render_template,request, redirect, url_for
+#Подключение библиотеки баз данных
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///diary.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email = db.Column(db.String(100), nullable=False)
+    text = db.Column(db.String(300), nullable=False)
+
+with app.app_context():
+    db.create_all()
 
 #Запуск страницы с контентом
 @app.route('/')
@@ -18,21 +31,24 @@ def process_form():
     button_discord = request.form.get('button_discord')
     button_html = request.form.get('button_html')
     button_db = request.form.get('button_db')
-    return render_template('index.html', button_python=button_python,button_discord=button_discord,button_html=button_html,button_db=button_db)
-
-@app.route('/submit', methods=['POST'])
-def submit_form():
-    #Создай переменные для сбора информации
-    email = request.form['email']
-    text = request.form['text']
+    button_unity = request.form.get('button_unity')
+    return render_template('index.html', button_python=button_python, button_discord=button_discord, button_html=button_html, button_db=button_db, button_unity=button_unity)
 
 
-    with open('form.txt', 'a', encoding= 'utf-8') as f:
-            f.write('ПОЧТА: ' + email + '\n')
-            f.write('КОММЕНТАРИЙ: ' + text + '\n')     
-    return 'Form submitted successfully'
 
+@app.route('/feedback', methods=['GET','POST'])
+def reg():
+    if request.method == 'POST':
+        email= request.form['email']
+        text = request.form['text']
+        
+        #Задание №3. Реализовать запись пользователей
+        
+        user = User(email=email, text=text)
+        db.session.add(user)
+        db.session.commit()
 
+        return redirect(url_for('index'))
 
 if __name__ == "__main__":
     app.run(debug=True)
